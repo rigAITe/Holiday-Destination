2491d1f5b954b1e8c1866cba08bf01e81153a50d
Tunmishe Israel <aitboy18@gmail.com>
2021-01-29 15:32:24 +0100

    make it better

--- src/components/mainHeader.js
+++ src/components/mainHeader.js
@@ -10,7 +10,7 @@ const MainHeader = () => {
             <div className="inner-header">
                 <div className="reveal">
                     <h2><Typical
-                        steps={["Let's Make Your Best Trip Ever", 1000,' Don’t Listen to what they say, Go see ', 3000]}
+                        steps={[" ", 1000,"Let's Make Your Best Trip Ever", 3000]}
                         loop={Infinity}
                         // wrapper="p"
                         /> 
